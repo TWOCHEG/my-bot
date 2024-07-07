25a841f03b7f132ac49d194ea8cbb9786a7de368
TWOCHEG <150810031+TWOCHEG@users.noreply.github.com>
2024-07-07 18:25:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,10 @@ from disnake.ext import commands
 import config
 
 from modules.Ready import BotReady
-from FluffBot.modules.discord.event import (CooldownError, ButtonClickRole, BotMetion, MemberJoin, MemberRemove, MessageRicRoll)
+from FluffBot.modules.discord.event import (CooldownError, ButtonClickRole, MessageJuniper, BotMetion,
+                                            MemberJoin, MemberRemove, MessageRicRoll)
 
-from FluffBot.modules.discord.command import (Clicker, Roulette, Sex, CPU, Currency, Shoot, Send, Profile)
+from FluffBot.modules.discord.command import (Clicker, Roulette, Sex, bot, Currency, Shoot, Send, Profile)
 from FluffBot.modules.discord.command.IQUp.IQMathTest import Test
 
 from FluffBot.modules.FanModules.currency.CurrencyList import CurrencyList
@@ -26,7 +27,7 @@ FluffBot.add_cog(CurrencyList(FluffBot))  # список балансов
 FluffBot.add_cog(CooldownError.CooldownError(FluffBot))  # cooldown error
 
 # cogs slash_command
-FluffBot.add_cog(CPU.CpuCommand(FluffBot))  # / CPU
+FluffBot.add_cog(bot.botCommand(FluffBot))  # / bot
 FluffBot.add_cog(Shoot.ShootCommand(FluffBot))  # / расстрелять
 FluffBot.add_cog(Roulette.RouletteCommand(FluffBot))  # / рулетка
 FluffBot.add_cog(Sex.SexCommand(FluffBot))  # / переспать
@@ -34,6 +35,7 @@ FluffBot.add_cog(Profile.ProfileCommand(FluffBot))  # / профиль
 FluffBot.add_cog(Send.SendCommand(FluffBot))  # / отправить сообщение\встраивание
 FluffBot.add_cog(Clicker.ClickCommand(FluffBot))  # / clicker
 FluffBot.add_cog(Currency.CurrencyCommand(FluffBot))  # / баланс
+FluffBot.add_cog(Test(FluffBot))  # / iq
 
 # cog event
 FluffBot.add_cog(Verification(FluffBot))  # верификация
@@ -48,6 +50,8 @@ FluffBot.add_cog(AntiSpam.AntiSpam(FluffBot))  # анти спам
 
 # ответы на сообщения
 FluffBot.add_cog(MessageRicRoll.RicRoll(FluffBot))  # ответ на рик рол
+FluffBot.add_cog(MessageJuniper.juniper(FluffBot))  # ответ на сообщение JuniperBot
+FluffBot.add_cog(BotMetion.Metion(FluffBot))  # упоминание бота (в разработке)
 FluffBot.add_cog(IQEv(FluffBot))  # IQ
 
 FluffBot.run(config.TOKEN)
